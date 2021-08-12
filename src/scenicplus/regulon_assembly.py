f75c7ced591ad81e7699af6068e74b72a88cecb5
sdewin <seppe.dewinter@hotmail.com>
2021-08-12 16:21:47 +0200

    use modules instead of adjacencies

--- src/scenicplus/regulon_assembly.py
+++ src/scenicplus/regulon_assembly.py
@@ -3,52 +3,16 @@ import numpy as np
 import logging
 import sys
 from .gsea import run_gsea
-import ray
+from tqdm import tqdm
+from pyscenic.utils import ACTIVATING_MODULE, REPRESSING_MODULE
 
 def assemble_e_regulons(
-    df_region2gene: pd.DataFrame,
-    df_tf2gene: pd.DataFrame,
-    df_tf2region: pd.DataFrame,
-    ray_n_cpu = None,
-    sort_by = 'importance',
-    n_perm = 1000,
-    GSEA_NES_thr = 3,
-    GSEA_pVal_thr = 0.05,
-    **kwargs):
-    """
-    Assembles final eRegulons by linking regions in cistromes to genes but only keeping genes which are enriched in the top of the TF-to-gene adjecency ranking.
-    
-    Parameters
-    ----------
-    df_region2gene: pd.DataFrame
-        A :class: `pd.DataFrame` containing region-to-gene links (should be already filtered). 
-        Should minimally contain following columns: "region", "gene", "importance", "rho", "aggr_rank_score". Extra columns are allowed.
-    df_tf2gene: pd.DataFrame
-        A :class: `pd.DataFrame` containing tf-to-gene adjecencies (should be already filtered).
-        Should minimally contain following columns: "TF", "gene", "importance", "rho", "regulation". Extra columns are allowed
-    df_tf2region: pd.DataFrame
-        A :class: `pd.DataFrame` containing tf-to-region links based on motif enrichment (should be already filtered).
-        Should minimally contain following columns: "TF", "region", "max_nes". Extra columns are allowed
-    ray_n_cpu: int
-        Number of cores to use when performing GSEA. Default is None (i.e. single core).
-    sort_by: str
-        Key by which to sort the tf-to-gene adjecencies to generate a ranking for GSEA. Default is "importance".
-        When the key is equal to "rho_abs", an extra column containing absolute correlation coefficient values is generated to create the ranking.
-    n_perm: int
-        Number of permutations to perform when empirically estimating the GSEA p value. Default is 1000.
-    GSEA_NES_thr: int
-        Mininum GSEA Normalized Enrichment Score (NES) value to retain. Default is 3.
-    GSEA_pVal_thr: float
-        Maximum GSEA p value to retain. Default is 0.05.
-    kwargs
-        Extra keyword arguments passed to ray.init()
-    
-    Returns
-    ------
-    pd.DataFrame
-        A :class: `pd.DataFrame` containing the final eRegulon table.
-    """
-
+    modules: list,
+    df_cistromes: pd.DataFrame,
+    df_region_to_gene: pd.DataFrame,
+    n_perm: int = 5000,
+    GSEA_NES_thr: float = 0.0,
+    GSEA_PVal_thr: float = 0.01):
     # Create logger
     level    = logging.INFO
     format   = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
@@ -56,71 +20,53 @@ def assemble_e_regulons(
     logging.basicConfig(level = level, format = format, handlers = handlers)
     log = logging.getLogger('ASM')
 
-    #link cistrome regions to genes
+    # Link regions of citromes to genes
     log.info('Linking cistromes to genes.')
-    df_cistrome_to_gene = df_tf2region.merge(df_region2gene, on = 'region')
+    df_cistrome_to_gene = df_cistromes.merge(df_region_to_gene, on = 'region')
 
-    #perform gsea by TF with cistrome genes as geneset and TF2Gene adj as ranking
-    log.info('Performing GSEA to test which genes linked to region in TF cistrome are enriched in the top of the TF-to-gene adjacency ranking.')
-    TFs = list(set(df_tf2gene['TF']) & set(df_cistrome_to_gene['TF']))
+    # Selected relevant modules
+    module_TFs = [module.transcription_factor for module in modules]
+    uniq_module_TFs = set(module_TFs)
+    uniq_cistrome_TFs = set(df_cistrome_to_gene['TF'])
+    cistrome_TFs_w_module = uniq_module_TFs & uniq_cistrome_TFs
+    log.info('{} of the {} cistrome transcription factors has a transcription factor to gene module.'.format(
+        len(cistrome_TFs_w_module), len(uniq_cistrome_TFs)))
+    #convert list of modules to np.array to allow indexing using boolean array
+    relevant_modules = list( np.array(modules)[ np.isin(module_TFs, list(cistrome_TFs_w_module)) ] ) 
 
-    if sort_by == 'abs_rho':
-        df_tf2gene['abs_rho'] = abs(df_tf2gene['rho'])
+    #sanity check
+    assert len( set([module.transcription_factor for module in relevant_modules]) & cistrome_TFs_w_module) == len(cistrome_TFs_w_module)
+
+    log.info('Performing GSEA for each module to test which genes linked to region in the associated TF cistrome are enriched in the top of the module.')
     
-    if ray_n_cpu is None:
-        dict_GSEA_result = {}
-        from tqdm import tqdm
-        for TF in tqdm(TFs, total = len(TFs)):
-            rnk = df_tf2gene.loc[df_tf2gene['TF'] == TF].sort_values(sort_by, ascending = False)
-            gene_set = list(set(df_cistrome_to_gene.loc[df_cistrome_to_gene['TF'] == TF, 'gene']))
+    results = []
+    for module in tqdm(relevant_modules, total = len(relevant_modules)):
+        TF  = module.transcription_factor
+        rnk = pd.Series(module.gene2weight)
+        rnk.loc[TF] = max(rnk) + 1 # put the TF itself in the first position of the ranking 
+        rnk = rnk.sort_values(ascending = False)
+        TF_cistrome = df_cistrome_to_gene.loc[ df_cistrome_to_gene['TF'] == TF ]
+        regulation = 1 if ACTIVATING_MODULE in module.context else -1 if REPRESSING_MODULE in module.context else None
+        assert regulation is not None
+        for group in set( TF_cistrome['group'] ):
+            TF_cistrome_group = TF_cistrome.loc[ TF_cistrome['group']  == group]
+            gene_set = list( set( TF_cistrome_group['gene'] ) )
             NES, pval, LE_genes = run_gsea(
-                ranked_gene_list = rnk['gene'].to_numpy(),
+                ranked_gene_list = rnk.index.to_numpy(),
                 gene_set = gene_set,
                 n_perm = n_perm)
-            dict_GSEA_result[TF] = (NES, pval, LE_genes)
-    else:
-        @ray.remote
-        def _ray_run_gsea(ranked_gene_list, gene_set, weights = None, p = 0, n_perm = 1000, return_res = False):
-            return run_gsea(ranked_gene_list, gene_set, weights, p, n_perm, return_res)
-        ray.init(num_cpus = ray_n_cpu, **kwargs)
-        try:
-            jobs = []
-            for TF in TFs:
-                rnk = df_tf2gene.loc[df_tf2gene['TF'] == TF].sort_values(sort_by, ascending = False)
-                gene_set = list(set(df_cistrome_to_gene.loc[df_cistrome_to_gene['TF'] == TF, 'gene']))
-                jobs.append(
-                    _ray_run_gsea.remote(
-                        ranked_gene_list = rnk['gene'].to_numpy(),
-                        gene_set = gene_set,
-                        n_perm = n_perm
-                    ))
-            l_GSEA_result = ray.get(jobs)
-        except Exception as e:
-            print(e)
-        finally:
-            ray.shutdown()
-        #transform list of GSEA results in dict
-        dict_GSEA_result = {TF: gsea_result for TF, gsea_result in zip(TFs, l_GSEA_result)}
+            results.append( (TF, group, NES, pval, LE_genes, regulation) )
+    df_results = pd.DataFrame(results, columns = ['TF', 'group', 'gsea_NES', 'gsea_pval', 'LE_genes', 'regulation'])
+    df_sign_results = df_results.loc[np.logical_and(df_results['gsea_pval'] <= GSEA_PVal_thr,  
+                                                    df_results['gsea_NES']  >= GSEA_NES_thr)]
+    df_sign_results = df_sign_results.explode('LE_genes')
+    df_sign_results.rename({'LE_genes': 'gene'}, inplace = True, axis = 1)
 
-    #Transform dict dict_GSEA_result in dataframe df_GSEA
-    df_GSEA = pd.DataFrame(dict_GSEA_result).T
-    df_GSEA.columns = ['GSEA_NES', 'GSEA_pval', 'gene']
-    df_GSEA['TF'] = df_GSEA.index
-    df_GSEA.reset_index(inplace = True)
-    df_GSEA.drop('index', axis = 1, inplace = True)
-    df_GSEA = df_GSEA.explode('gene')
-
-    #threshold on NES and pval
-    df_GSEA = df_GSEA.loc[
-        np.logical_and(
-            df_GSEA['GSEA_NES'] > GSEA_NES_thr,
-            df_GSEA['GSEA_pval'] < GSEA_pVal_thr)]
-
-    #combine results
     log.info('Subsetting for leading edge genes and generating final eRegulon dataframe.')
-    #subset cistrome to gene for leading edge genes
-    df_cistrome_to_gene_enr = df_cistrome_to_gene.merge(df_GSEA, on = ['TF', 'gene'])
-    df_final = df_cistrome_to_gene_enr.merge(df_tf2gene, on = ['TF', 'gene'], suffixes = ['_r2g', '_tf2g'])
+    eRegulons = df_sign_results.merge(df_cistrome_to_gene, on = ['TF', 'group', 'gene'], suffixes = ['_TF2G', '_R2G'])
+    #drop columns
+    eRegulons.drop(['gsea_NES', 'gsea_pval'], axis = 1, inplace = True)
+    #drop duplicates (coming from different module contexts)
+    eRegulons = eRegulons.drop_duplicates()
     log.info('Done!')
-    return df_final
-
+    return eRegulons
