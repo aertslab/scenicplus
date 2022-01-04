a78ae789146715dcdb86b13e647b677c8222e2f9
Carmen Bravo <carmen.bravogonzalezblas@kuleuven.be>
2022-01-04 16:33:31 +0100

    eRegulon binarization

--- src/scenicplus/eregulon_enrichment.py
+++ src/scenicplus/eregulon_enrichment.py
@@ -1,9 +1,22 @@
 from pycisTopic.diff_features import *
 from pycisTopic.signature_enrichment import *
+from pyscenic.binarization import binarize
 
 def get_eRegulons_as_signatures(scplus_obj: 'SCENICPLUS',
                               eRegulon_metadata_key: str ='eRegulon_metadata', 
                               key_added: str = 'eRegulon_signatures'):
+    """
+    Format eRegulons for scoring
+    
+    Parameters
+    ----------
+    scplus_obj: `class::SCENICPLUS`
+        A SCENICPLUS object with eRegulons metadata computed.
+    eRegulons_metadata_key: str, optional
+    	Key where the eRegulon metadata is stored (in `scplus_obj.uns`)
+    key_added: str, optional
+    	Key where formated signatures will be stored (in `scplus_obj.uns`)
+    """
     region_signatures = {x: list(set(scplus_obj.uns[eRegulon_metadata_key][scplus_obj.uns[eRegulon_metadata_key].Region_signature_name == x]['Region'])) for x in list(set(scplus_obj.uns[eRegulon_metadata_key].Region_signature_name))}
     gene_signatures = {x: list(set(scplus_obj.uns[eRegulon_metadata_key][scplus_obj.uns[eRegulon_metadata_key].Gene_signature_name == x]['Gene'])) for x in list(set(scplus_obj.uns[eRegulon_metadata_key].Gene_signature_name))}
 
@@ -22,7 +35,28 @@ def score_eRegulons(scplus_obj: 'SCENICPLUS',
                     auc_threshold: float = 0.05,
                     normalize: bool = False,
                     n_cpu: int = 1):
-
+    """
+    Score eRegulons using AUCell
+    
+    Parameters
+    ----------
+    scplus_obj: `class::SCENICPLUS`
+        A SCENICPLUS object with formatted eRegulons.
+    eRegulons_metadata_key: `class::CistopicImputedFeatures`
+    	Precomputed region/gene ranking.
+    eRegulon_signatures_key: str, optional
+    	Key where formated signatures are stored (in `scplus_obj.uns`)
+    key_added: str, optional
+    	Key where formated AUC values will be stored (in `scplus_obj.uns`)
+    enrichment_type: str, optional
+    	Whether region or gene signatures are being used
+    auc_threshold: float
+        The fraction of the ranked genome to take into account for the calculation of the Area Under the recovery Curve. Default: 0.05
+    normalize: bool
+        Normalize the AUC values to a maximum of 1.0 per regulon. Default: False
+    n_cpu: int
+        The number of cores to use. Default: 1
+    """
     if not key_added in scplus_obj.uns.keys():
         scplus_obj.uns[key_added] = {}
     
@@ -37,3 +71,31 @@ def score_eRegulons(scplus_obj: 'SCENICPLUS',
                         auc_threshold = auc_threshold,
                         normalize = normalize,
                         n_cpu = n_cpu)
+
+def binarize_AUC(scplus_obj: 'SCENICPLUS', 
+                 auc_key: Optional[str] = 'eRegulon_AUC', 
+                 out_key: Optional[str] = 'eRegulon_AUC_thresholds', 
+                 signature_keys: Optional[List[str]] = ['Gene_based', 'Region_based'],
+                 n_cpu: Optional[int] = 1):
+    """
+    Binarize eRegulons using AUCell
+    
+    Parameters
+    ----------
+    scplus_obj: `class::SCENICPLUS`
+        A SCENICPLUS object with eRegulons AUC.
+	auc_key: str, optional
+    	Key where the AUC values are stored
+    out_key: str, optional
+    	Key where the AUCell thresholds will be stored (in `scplus_obj.uns`)
+    signature_keys: List, optional
+        Keys to extract AUC values from. Default: ['Gene_based', 'Region_based']
+    n_cpu: int
+        The number of cores to use. Default: 1
+    """
+    if not out_key in scplus_obj.uns.keys():
+        scplus_obj.uns[out_key] = {}
+    for signature in signature_keys:  
+        auc_mtx = scplus_obj.uns[auc_key][signature]
+        _, auc_thresholds = binarize(auc_mtx, num_workers=n_cpu)
+        scplus_obj.uns[out_key][signature] = auc_thresholds
