76ebe1a85551301e9b8620e4840978b490dd5563
sdewin <seppe.dewinter@hotmail.com>
2022-01-07 10:39:49 +0100

    Change x-axis name to eRegulon rank

--- src/scenicplus/RSS.py
+++ src/scenicplus/RSS.py
@@ -134,7 +134,7 @@ def plot_rss(scplus_obj: 'SCENICPLUS',
         ax.set_xlabel('')
         adjust_text(ax.texts, autoalign='xy', ha='right', va='bottom', arrowprops=dict(arrowstyle='-',color='lightgrey'), precision=0.001 )
         if num_columns == 1:
-            fig.text(0.5, 0.0, 'eRegulon', ha='center', va='center', size='x-large')
+            fig.text(0.5, 0.0, 'eRegulon rank', ha='center', va='center', size='x-large')
             fig.text(0.00, 0.5, 'eRegulon specificity score (eRSS)', ha='center', va='center', rotation='vertical', size='x-large')
             plt.tight_layout()
             plt.rcParams.update({
