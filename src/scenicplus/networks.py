de1ab6aec61591acedfc74346c96b7d072af7ce4
Carmen Bravo <carmen.bravogonzalezblas@kuleuven.be>
2022-01-11 15:24:45 +0100

    Add layouts

--- src/scenicplus/networks.py
+++ src/scenicplus/networks.py
@@ -328,3 +328,209 @@ def create_nx_graph(nx_tables,
     font_nt_d = font_nt_d.to_dict(orient='index')
     nx.set_node_attributes(G, font_nt_d, name='font')
     return G, edge_tables, node_tables
+
+
+def _distance(p1, p2):
+    """
+    Helper function for custom layout
+    """
+    x1 = p1[0]
+    x2 = p2[0]
+    y1 = p1[1]
+    y2 = p2[1]
+
+    return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+def _pairwise_distance(points):
+    """
+    Helper function for custom layout
+    """
+    distances = np.zeros((points.shape[0], points.shape[0]))
+    for i in range(points.shape[0]):
+        for j in range(points.shape[0]):
+            distances[i, j] = _distance(points[i], points[j])
+    np.fill_diagonal(distances, np.NINF)
+    return distances
+
+
+def _line_two_points(p1, p2, return_func=True):
+    """
+    Helper function for custom layout
+    """
+    x1 = p1[0]
+    x2 = p2[0]
+    y1 = p1[1]
+    y2 = p2[1]
+
+    m = (y2 - y1) / (x2 - x1)
+    b = y1 - m * x1
+    if return_func:
+        return lambda x: m * x + b
+    else:
+        return m, b
+
+
+def _line_slope_point(m, p, return_func=True):
+    """
+    Helper function for custom layout
+    """
+    x = p[0]
+    y = p[1]
+    b = y - m * x
+
+    if return_func:
+        return lambda x: m * x + b
+    else:
+        return m, b
+
+def concentrical_layout(G,
+               dist_genes=1,
+               dist_TF=0.1):
+    """
+    Generate custom concentrical layout
+    
+    Parameters
+    ---------
+    G: Graph
+        A networkx graph
+    dist_genes: int, optional
+        Distance from the regions to the genes
+    dist_TF
+        Distance from the TF to the regions
+    """
+    node_type = nx.get_node_attributes(G, 'group')
+    TF_nodes = [n for n in G.nodes if node_type[n] == 'TF']
+    region_nodes = [n for n in G.nodes if node_type[n] == 'Region']
+    gene_nodes = [n for n in G.nodes if node_type[n] == 'Gene']
+
+    # get regions with TFs as target
+    tmp = pd.DataFrame(list(G.edges))
+    regions_targetting_TFs = tmp.loc[np.isin(tmp[1], TF_nodes), 0].to_list()
+    del(tmp)
+    region_nodes = list(set(region_nodes) - set(regions_targetting_TFs))
+
+    # layout regions in a circle
+    n_region_nodes = len(region_nodes)
+
+    theta = np.linspace(0, 1, n_region_nodes + 1)[:-1] * 2 * np.pi
+    theta = theta.astype(np.float32)
+    pos_regions = np.column_stack(
+        [np.cos(theta), np.sin(theta), np.zeros((n_region_nodes, 0))]
+    )
+
+    # sort regions by target
+    source_target_dict = {}
+    target_source_dict = {}
+    for edge in G.edges:
+        source = edge[0]
+        target = edge[1]
+
+        if source in source_target_dict.keys():
+            if target not in source_target_dict[source]:
+                source_target_dict[source].append(target)
+        else:
+            source_target_dict[source] = [target]
+
+        if target in target_source_dict.keys():
+            if source not in target_source_dict[target]:
+                target_source_dict[target].append(source)
+        else:
+            target_source_dict[target] = [source]
+
+    region_nodes = sorted(region_nodes, key=lambda x: target_source_dict[x][0])
+    pos_regions = dict(zip(region_nodes, pos_regions))
+
+    # layout target genes in concentric circle around regions
+    pos_genes = {}
+    additional_genes_to_position = []
+    for gene in gene_nodes:
+        # get regions targetting this gene and their position
+        regions = target_source_dict[gene]
+        if all([r in regions_targetting_TFs for r in regions]):
+            additional_genes_to_position.append(gene)
+            continue
+        pos_regions_gene = np.array(
+            [pos_regions[r] for r in regions if r not in regions_targetting_TFs])
+        if len(regions) > 1:
+            # get the positions which are furthest apart and "draw" a line through them
+            pairwise_distances = _pairwise_distance(pos_regions_gene)
+            furthest_points = np.unravel_index(
+                pairwise_distances.argmax(), pairwise_distances.shape)
+            m, b = _line_two_points(
+                pos_regions_gene[furthest_points[0]], pos_regions_gene[furthest_points[1]], return_func=False)
+            # draw a perpendicular line through the first line and the mean position
+            p_mean = pos_regions_gene.mean(0)
+            m, b = _line_slope_point(-1/m, p_mean, return_func=False)
+            # get the point which is a distance dist_genes away from point p_mean
+            p_new = [p_mean[0] - dist_genes * np.sqrt(1 / (1 + m**2)),
+                     p_mean[1] - m * dist_genes * np.sqrt(1 / (1 + m**2))]
+            # check if point is within the circle, otherwise take the other point (radius of the circle is 1)
+            if p_new[0]**2 + p_new[1]**2 < 1:
+                p_new = [p_mean[0] + dist_genes * np.sqrt(1 / (1 + m**2)),
+                         p_mean[1] + m * dist_genes * np.sqrt(1 / (1 + m**2))]
+        else:
+            # draw line trough origin and pos of region
+            m, b = _line_two_points(pos_regions_gene[0], [
+                                    0, 0], return_func=False)
+            # get the point which is a distance dist_genes away from point pos_regions_gene
+            p_new = [pos_regions_gene[0][0] - dist_genes * np.sqrt(1 / (1 + m**2)),
+                     pos_regions_gene[0][1] - m * dist_genes * np.sqrt(1 / (1 + m**2))]
+            # check if point is within the circle, otherwise take the other point (radius of the circle is 1)
+            if p_new[0]**2 + p_new[1]**2 < 1:
+                p_new = [pos_regions_gene[0][0] + dist_genes * np.sqrt(1 / (1 + m**2)),
+                         pos_regions_gene[0][1] + m * dist_genes * np.sqrt(1 / (1 + m**2))]
+        pos_genes[gene] = np.array(p_new)
+
+    pos_TF = {}
+    for TF in TF_nodes:
+        # get regions targetted by this TF and their position
+        regions = source_target_dict[TF]
+        if all([r in regions_targetting_TFs for r in regions]):
+            additional_genes_to_position.append(TF)
+            continue
+        pos_regions_TF = np.array(
+            [pos_regions[r] for r in regions if r not in regions_targetting_TFs])
+        if len(regions) > 1:
+            # get the positions which are furthest apart and "draw" a line through them
+            pairwise_distances = _pairwise_distance(pos_regions_TF)
+            furthest_points = np.unravel_index(
+                pairwise_distances.argmax(), pairwise_distances.shape)
+            m, b = _line_two_points(
+                pos_regions_TF[furthest_points[0]], pos_regions_TF[furthest_points[1]], return_func=False)
+            # draw a perpendicular line through the first line and the mean position
+            p_mean = pos_regions_TF.mean(0)
+            m, b = _line_slope_point(-1/m, p_mean, return_func=False)
+            # get the point which is a distance dist_genes away from point p_mean
+            p_new = [p_mean[0] - dist_TF * np.sqrt(1 / (1 + m**2)),
+                     p_mean[1] - m * dist_TF * np.sqrt(1 / (1 + m**2))]
+            # check if point is within the circle, otherwise take the other point (radius of the circle is 1)
+            if p_new[0]**2 + p_new[1]**2 > 1:
+                p_new = [p_mean[0] + dist_TF * np.sqrt(1 / (1 + m**2)),
+                         p_mean[1] + m * dist_TF * np.sqrt(1 / (1 + m**2))]
+        else:
+            # draw line trough origin and pos of region
+            m, b = _line_two_points(
+                pos_regions_TF[0], [0, 0], return_func=False)
+            # get the point which is a distance dist_genes away from point pos_regions_gene
+            p_new = [pos_regions_TF[0][0] - dist_TF * np.sqrt(1 / (1 + m**2)),
+                     pos_regions_TF[0][1] - m * dist_TF * np.sqrt(1 / (1 + m**2))]
+            # check if point is within the circle, otherwise take the other point (radius of the circle is 1)
+            if p_new[0]**2 + p_new[1]**2 < 1:
+                p_new = [pos_regions_TF[0][0] + dist_TF * np.sqrt(1 / (1 + m**2)),
+                         pos_regions_TF[0][1] + m * dist_TF * np.sqrt(1 / (1 + m**2))]
+        pos_TF[TF] = np.array(p_new)
+
+    # layout TF nodes within circle
+    #G_TF = G.subgraph(nodes = [*TF_nodes, *regions_targetting_TFs, *additional_genes_to_position])
+    #additional_genes_to_position_init = {gene: [random.uniform(0, 1), random.uniform(0, 1)] for gene in additional_genes_to_position}
+    #pos_TF = nx.spring_layout(G_TF, scale = 0.7, pos = {**pos_TF, **additional_genes_to_position_init})
+    G_add = G.subgraph(nodes=additional_genes_to_position)
+    pos_add = nx.spring_layout(G_add, scale=0.1)
+
+    G_regions_TF = G.subgraph(nodes=[*regions_targetting_TFs, *TF_nodes])
+    pos_regions_TF = nx.spring_layout(G_regions_TF, scale=1)
+    pos_regions_TF = {k: pos_regions_TF[k] for k in pos_regions_TF.keys(
+    ) if k in regions_targetting_TFs}
+
+    return {**pos_TF, **pos_regions, **pos_genes, **pos_add, **pos_regions_TF}
