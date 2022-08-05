22c803a4dc253f3246f0926ba2081be44038f2ee
sdewin <seppe.dewinter@hotmail.com>
2022-08-05 15:09:52 +0200

    generate dotplot using plotnine

--- src/scenicplus/plotting/dotplot.py
+++ src/scenicplus/plotting/dotplot.py
@@ -13,12 +13,252 @@ from typing import Dict, List, Tuple
 from typing import Optional
 import re
 from pycistarget.motif_enrichment_dem import DEM
+from plotnine import ggplot, geom_point, aes, scale_fill_distiller, theme_bw, geom_tile, theme, element_text, element_blank
+from plotnine.facets import facet_grid
+import plotnine
+
 
 from ..scenicplus_class import SCENICPLUS
+from ..utils import flatten_list
+
+def generate_dotplot_df(
+    scplus_obj: SCENICPLUS,
+    size_matrix: pd.DataFrame,
+    color_matrix: pd.DataFrame,
+    scale_size_matrix: bool = True,
+    scale_color_matrix: bool = True,
+    group_variable: str = None,
+    subset_eRegulons: list = None) -> pd.DataFrame:
+    """
+    Function to generate dotplot dataframe from cistrome AUC enrichment
+
+    Parameters
+    ----------
+    scplus_obj: `class::SCENICPLUS`
+        A :class:`SCENICPLUS` object.
+    size_matrix: pd.DataFrame
+        A pd.DataFrame containing values to plot using size scale.
+    color_matrix
+        A pd.DataFrame containing values to plot using color scale.
+    scale_size_matrix: bool
+        Scale size matrix between 0 and 1 along index.
+    scale_color_matrix: bool
+        Scale color matrix between 0 and 1 along index.
+    group_variable: str:
+        Variable by which to group cell barcodes by (needed if the index of size or color matrix are cells.)
+    subset_eRegulons: List
+        List of eRegulons to plot.
+    Returns
+    -------
+    pd.DataFrame
+    """
+    # Handle matrices which have cell barcodes on one axis
+    if all(np.isin(size_matrix.columns, scplus_obj.cell_names)):
+        size_matrix = size_matrix.T
+    if all(np.isin(size_matrix.index, scplus_obj.cell_names)):
+        #calculate mean values
+        if group_variable is None:
+            raise ValueError('group_variable can not be None when size_matrix is a matrix with cell barcodes on one axis.')
+        if group_variable not in scplus_obj.metadata_cell.columns:
+            raise ValueError('group variable must be a column in scplus_obj.metadata_cell')
+        size_matrix = size_matrix.groupby(scplus_obj.metadata_cell[group_variable]).mean()
+    if all(np.isin(color_matrix.columns, scplus_obj.cell_names)):
+        color_matrix = color_matrix.T
+    if all(np.isin(color_matrix.index, scplus_obj.cell_names)):
+        #calculate mean values
+        if group_variable is None:
+            raise ValueError('group_variable can not be None when color_matrix is a matrix with cell barcodes on one axis.')
+        if group_variable not in scplus_obj.metadata_cell.columns:
+            raise ValueError('group variable must be a column in scplus_obj.metadata_cell')
+        color_matrix = color_matrix.groupby(scplus_obj.metadata_cell[group_variable]).mean()
+    if not all(np.isin(size_matrix.index, color_matrix.index)):
+        raise ValueError('size_matrix and color_matrix should have the same values as index.')
+    size_matrix_features = (
+        size_matrix.columns.to_list(),                      #full eRegulon name
+        [f.split('_(')[0] for f in size_matrix.columns],    #eRegulon name without number of targets
+        [f.split('_')[0] for f in size_matrix.columns])     #tf name
+    color_matrix_features = (
+        color_matrix.columns.to_list(),                      #full eRegulon name
+        [f.split('_(')[0] for f in color_matrix.columns],    #eRegulon name without number of targets
+        [f.split('_')[0] for f in color_matrix.columns])     #tf name
+
+    #put features in same order
+    #both matrices have eRegulons as features
+    def _find_idx(l, e):
+        return [i for i, v in enumerate(l) if v == e]
+    if all(['_(' in x for x in size_matrix_features[0]]) and all(['_(' in x for x in color_matrix_features[0]]):
+        if not all(np.isin(size_matrix_features[1], color_matrix_features[1])):
+            raise ValueError('When two matrices are given with eRegulons as features then the names of these features (without number of targets) should match!')
+        color_matrix = color_matrix.iloc[
+            :, flatten_list([_find_idx(color_matrix_features[1], e) for e in size_matrix_features[1]])]
+    #size matrix has eRegulons as features but color matrix not
+    elif all(['_(' in x for x in size_matrix_features[0]]):
+        color_matrix = color_matrix[size_matrix_features[2]]
+    #color matrix has eRegulons as features but size matrix not
+    elif all(['_(' in x for x in color_matrix_features[0]]):
+        size_matrix = size_matrix[color_matrix_features[2]]
+    #none of the matrices have eRegulons as features
+    else:
+        size_matrix = size_matrix[color_matrix_features[2]]
+    
+    if subset_eRegulons is not None:
+        #change to TF names
+        subset_eRegulons = [x.split('_')[0] for x in subset_eRegulons]
+        size_matrix = size_matrix[[x for x in size_matrix if x.split('_')[0] in subset_eRegulons]]
+        color_matrix = color_matrix[[x for x in color_matrix if x.split('_')[0] in subset_eRegulons]]
+    
+    if scale_size_matrix:
+        size_matrix = (size_matrix - size_matrix.min()) / (size_matrix.max() - size_matrix.min())
+    if scale_color_matrix:
+        color_matrix = (color_matrix - color_matrix.min()) / (color_matrix.max() - color_matrix.min())
+    
+    size_matrix_df = size_matrix.stack().reset_index()
+    color_matrix_df = color_matrix.stack().reset_index()
+    size_matrix_df.columns = ['index', 'size_name', 'size_val']
+    color_matrix_df.columns = ['index', 'color_name', 'color_val']
+    if all(['_(' in x for x in size_matrix_features[0]]) and all(['_(' in x for x in color_matrix_features[0]]):
+        size_matrix_df['eRegulon_name'] = [x.split('_(')[0] for x in size_matrix_df['size_name']]
+        color_matrix_df['eRegulon_name'] = [x.split('_(')[0] for x in color_matrix_df['color_name']]
+        merged_df = size_matrix_df.merge(color_matrix_df, on = ['index', 'eRegulon_name'])
+        merged_df['TF'] = [x.split('_')[0] for x in merged_df['eRegulon_name']]
+    else:
+        size_matrix_df['TF'] = [x.split('_')[0] for x in size_matrix_df['size_name']]
+        color_matrix_df['TF'] = [x.split('_')[0] for x in color_matrix_df['color_name']]
+        merged_df = size_matrix_df.merge(color_matrix_df, on = ['index', 'TF'])
+        if all(['_(' in x for x in size_matrix_features[0]]):
+            merged_df['eRegulon_name'] = merged_df['size_name']
+        elif all(['_(' in x for x in color_matrix_features[0]]):
+            merged_df['eRegulon_name'] = merged_df['color_name']
+        else:
+            merged_df['eRegulon_name'] = merged_df['TF']
+
+    #for esthetics
+    merged_df = merged_df[['index', 'TF', 'eRegulon_name', 'size_name', 'color_name', 'size_val', 'color_val']]
+    return merged_df
+
+
+def heatmap_dotplot(
+    scplus_obj: SCENICPLUS,
+    size_matrix: pd.DataFrame,
+    color_matrix: pd.DataFrame,
+    scale_size_matrix: bool = True,
+    scale_color_matrix: bool = True,
+    group_variable: str = None,
+    subset_eRegulons: list = None,
+    sort_by: str = 'color_val',
+    index_order: list = None,
+    save: str = None,
+    figsize: tuple = (5, 8),
+    split_repressor_activator: bool = True,
+    orientation: str = 'vertical'):
+    """
+    Function to generate dotplot dataframe from cistrome AUC enrichment
+
+    Parameters
+    ----------
+    scplus_obj: `class::SCENICPLUS`
+        A :class:`SCENICPLUS` object.
+    size_matrix: pd.DataFrame
+        A pd.DataFrame containing values to plot using size scale.
+    color_matrix
+        A pd.DataFrame containing values to plot using color scale.
+    scale_size_matrix: bool
+        Scale size matrix between 0 and 1 along index.
+    scale_color_matrix: bool
+        Scale color matrix between 0 and 1 along index.
+    group_variable: str:
+        Variable by which to group cell barcodes by (needed if the index of size or color matrix are cells.)
+    subset_eRegulons: List
+        List of eRegulons to plot.
+    sort_by: str
+        Sort by color_val or size_val.
+    index_order: list
+        Order of index to plot.
+    figsize: tuple
+        size of the figure (x, y).
+    split_repressor_activator: bool
+        Wether to split the plot on repressors/activators.
+    orientation: str
+        Plot in horizontal or vertical orientation
+    """
+    plotting_df = generate_dotplot_df(
+        scplus_obj = scplus_obj,
+        size_matrix = size_matrix,
+        color_matrix = color_matrix,
+        scale_size_matrix = scale_size_matrix,
+        scale_color_matrix = scale_color_matrix,
+        group_variable = group_variable,
+        subset_eRegulons = subset_eRegulons)
+    if index_order is not None:
+        if len(set(index_order) & set(plotting_df['index'])) != len(set(plotting_df['index'])):
+            Warning('not all indices are provided in index_order, order will not be changed!')
+        else:
+            plotting_df['index'] = pd.Categorical(plotting_df['index'], categories = index_order)
+    #sort values
+    tmp = plotting_df[['index', 'eRegulon_name', sort_by]
+        ].pivot_table(index = 'index', columns = 'eRegulon_name'
+        ).fillna(0)['color_val']
+    if index_order is not None:
+        tmp = tmp.loc[index_order]
+    idx_max = tmp.idxmax(axis = 0)
+    order = pd.concat([idx_max[idx_max == x] for x in tmp.index.tolist() if len(plotting_df[plotting_df == x]) > 0]).index.tolist()
+    plotting_df['eRegulon_name'] = pd.Categorical(plotting_df['eRegulon_name'], categories = order)
+    plotnine.options.figure_size = figsize
+    if split_repressor_activator:
+        plotting_df['repressor_activator'] = ['activator' if '+' in n else 'repressor' for n in plotting_df['eRegulon_name']]
+        if orientation == 'vertical':
+            plot = (
+                ggplot(plotting_df, aes('index', 'eRegulon_name'))
+                + facet_grid(
+                    'repressor_activator ~ .', 
+                    scales = "free", 
+                    space = {'x': [1], 'y': [sum(plotting_df['repressor_activator'] == 'activator'), sum(plotting_df['repressor_activator'] == 'repressor')]})
+                + geom_tile(mapping = aes(fill = 'color_val'))
+                + scale_fill_distiller(type = 'div', palette = 'RdYlBu')
+                + geom_point(
+                        mapping = aes(size = 'size_val'),
+                        colour = "black")
+                + theme(axis_text_x=element_text(rotation=90, hjust=1))
+                + theme(axis_title_x = element_blank(), axis_title_y = element_blank()))
+        elif orientation == 'horizontal':
+            plot = (
+                ggplot(plotting_df, aes('eRegulon_name', 'index'))
+                + facet_grid(
+                    '. ~ repressor_activator', 
+                    scales = "free", 
+                    space = {'y': [1], 'x': [sum(plotting_df['repressor_activator'] == 'activator'), sum(plotting_df['repressor_activator'] == 'repressor')]})
+                + geom_tile(mapping = aes(fill = 'color_val'))
+                + scale_fill_distiller(type = 'div', palette = 'RdYlBu')
+                + geom_point(
+                        mapping = aes(size = 'size_val'),
+                        colour = "black")
+                + theme(axis_text_x=element_text(rotation=90, hjust=1))
+                + theme(axis_title_x = element_blank(), axis_title_y = element_blank()))
+    else:
+        if orientation == 'vertical':
+            plot = (
+                ggplot(plotting_df, aes('index', 'eRegulon_name'))
+                + geom_tile(mapping = aes(fill = 'color_val'))
+                + scale_fill_distiller(type = 'div', palette = 'RdYlBu')
+                + geom_point(
+                        mapping = aes(size = 'size_val'),
+                        colour = "black")
+                + theme(axis_title_x = element_blank(), axis_title_y = element_blank()))
+        elif orientation == 'horizontal':
+            plot = (
+                ggplot(plotting_df, aes('eRegulon_name', 'index'))
+                + geom_tile(mapping = aes(fill = 'color_val'))
+                + scale_fill_distiller(type = 'div', palette = 'RdYlBu')
+                + geom_point(
+                        mapping = aes(size = 'size_val'),
+                        colour = "black")
+                + theme(axis_title_x = element_blank(), axis_title_y = element_blank()))
+    if save is not None:
+        plot.save(save)
+    else:
+        return plot
 
 # Utils
-
-
 def _flatten(A):
     """
     Utils function to flatten lists
@@ -45,7 +285,7 @@ def generate_dotplot_df_motif_enrichment(scplus_obj: SCENICPLUS,
                                          normalize_expression: bool = True,
                                          standardize_expression: bool = False):
     """
-    Function to generate dotplot dataframe from motif enrichment results
+    DEPRECATED Function to generate dotplot dataframe from motif enrichment results
 
     Parameters
     ---------
@@ -177,19 +417,18 @@ def generate_dotplot_df_motif_enrichment(scplus_obj: SCENICPLUS,
     dotplot_df = dotplot_df.replace(np.nan, 0)
     return dotplot_df
 
-
 def generate_dotplot_df_AUC(scplus_obj: SCENICPLUS,
                             auc_key: str,
                             enrichment_key: str,
                             group_variable: str,
-                            subset: list = None,
-                            subset_cistromes: list = None,
+                            subset_cells: list = None,
+                            subset_eRegulons: list = None,
                             use_pseudobulk: bool = False,
                             normalize_expression: bool = True,
                             standardize_expression: bool = False,
                             standardize_auc: bool = False):
     """
-    Function to generate dotplot dataframe from cistrome AUC enrichment
+    DEPRECATED Function to generate dotplot dataframe from cistrome AUC enrichment
 
     Parameters
     ---------
@@ -200,9 +439,9 @@ def generate_dotplot_df_AUC(scplus_obj: SCENICPLUS,
     group_variable: str
         Group variable to use to calculate TF expression per group. Levels of this variable should match with the entries in the
         selected motif enrichment dictionary. 
-    subset: list, optional
+    subset_cells: list, optional
         Subset of classes to use. Default: None (use all)
-    subset_cistromes: list, optional
+    subset_eRegulons: list, optional
         List of cistromes to use. Default: None (use all)
     use_pseudobulk: bool, optional
         Whether to use pseudobulk to calculate gene expression. Only available if there is pseudobulk profiles for group_variable.
@@ -238,26 +477,26 @@ def generate_dotplot_df_AUC(scplus_obj: SCENICPLUS,
         dgem = dgem.T / dgem.T.sum(0) * 10**6
         dgem = np.log1p(dgem).T
     # Check for cistrome subsets
-    if subset_cistromes is None:
-        subset_cistromes = cistromes_auc.index.tolist()
+    if subset_eRegulons is None:
+        subset_eRegulons = cistromes_auc.index.tolist()
     # Check that cistromes are in the AUC matrix
-    subset_cistromes = set(subset_cistromes).intersection(
+    subset_eRegulons = set(subset_eRegulons).intersection(
         cistromes_auc.index.tolist())
     # Take TF names
     subset_tfs = [re.sub('_(.*)', '', (re.sub('_extended', '', cistrome_name)))
-                  for cistrome_name in subset_cistromes]
-    check_df = pd.DataFrame([subset_cistromes, subset_tfs]).T
+                  for cistrome_name in subset_eRegulons]
+    check_df = pd.DataFrame([subset_eRegulons, subset_tfs]).T
     check_df.columns = ['Subset_cistromes', 'Subset_TFs']
     check_df = check_df[check_df['Subset_TFs'].isin(dgem.index.tolist())]
     subset_tfs = list(set(check_df['Subset_TFs'].tolist()))
-    subset_cistromes = list(set(check_df['Subset_cistromes'].tolist()))
+    subset_eRegulons = list(set(check_df['Subset_cistromes'].tolist()))
     # Subset matrices
     # By cistrome
     tf_expr = dgem.loc[subset_tfs, :]
-    cistromes_auc_tf = cistromes_auc.loc[subset_cistromes, :]
+    cistromes_auc_tf = cistromes_auc.loc[subset_eRegulons, :]
     # By cells
-    if subset is not None:
-        subset_cells = cell_data[cell_data.isin(subset)].index.tolist()
+    if subset_cells is not None:
+        subset_cells = cell_data[cell_data.isin(subset_cells)].index.tolist()
         cell_data = cell_data.loc[subset_cells]
         tf_expr = tf_expr.loc[:, subset_cells]
         cistromes_auc_tf = cistromes_auc_tf.loc[:, subset_cells]
@@ -340,7 +579,7 @@ def dotplot(df_dotplot: pd.DataFrame,
             plotly_height=1000,
             save=None):
     """
-    Function to generate dotplot
+    DEPRECATED Function to generate dotplot
 
     Parameters
     ---------
