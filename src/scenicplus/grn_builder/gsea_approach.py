e00a9fe8c09b2f0cbfd9abc7fb265dfb9a752180
SeppeDeWinter <seppe.dewinter@hotmail.com>
2023-05-17 14:24:40 +0000

    Made build_grn compatible with new cistarget_wrangling code, also made it more modular.

--- src/scenicplus/grn_builder/gsea_approach.py
+++ src/scenicplus/grn_builder/gsea_approach.py
@@ -48,11 +48,14 @@ import logging
 import sys
 from tqdm import tqdm
 import ray
+import anndata
+from typing import Dict, List
 
-from ..scenicplus_class import SCENICPLUS
-from ..utils import p_adjust_bh
-from .modules import create_emodules, eRegulon, merge_emodules, RHO_THRESHOLD, TARGET_GENE_NAME
-from .gsea import run_gsea
+from scenicplus.scenicplus_class import SCENICPLUS
+from scenicplus.utils import p_adjust_bh
+from scenicplus.grn_builder.modules import (
+    create_emodules, eRegulon, merge_emodules, RHO_THRESHOLD, TARGET_GENE_NAME)
+from scenicplus.grn_builder.gsea import run_gsea
 
 # create logger
 level = logging.INFO
@@ -107,113 +110,37 @@ def _ray_run_gsea_for_e_module(e_module, rnk, gsea_n_perm, context):
     return _run_gsea_for_e_module(e_module, rnk, gsea_n_perm, context)
 
 
-def build_grn(SCENICPLUS_obj: SCENICPLUS,
-              adj_key='TF2G_adj',
-              cistromes_key='Unfiltered',
-              region_to_gene_key='region_to_gene',
-              order_regions_to_genes_by='importance',
-              order_TFs_to_genes_by='importance',
-              gsea_n_perm=1000,
-              quantiles=(0.85, 0.90),
-              top_n_regionTogenes_per_gene=(5, 10, 15),
-              top_n_regionTogenes_per_region=(),
-              binarize_using_basc=False,
-              min_regions_per_gene=0,
-              rho_dichotomize_tf2g=True,
-              rho_dichotomize_r2g=True,
-              rho_dichotomize_eregulon=True,
-              keep_only_activating=False,
-              rho_threshold=RHO_THRESHOLD,
-              NES_thr=0,
-              adj_pval_thr=1,
-              min_target_genes=5,
-              inplace=True,
-              key_added='eRegulons',
-              ray_n_cpu=None,
-              merge_eRegulons=True,
-              keep_extended_motif_annot=False,
-              disable_tqdm=False,
-              **kwargs):
-    """
-    Build GRN using GSEA approach
-
-    Parameters
-    ---------
-    SCENICPLUS_obj
-        An instance of :class: `~scenicplus.scenicplus_class.SCENICPLUS`
-    adj_key
-        Key under which to find TF2G adjacencies
-        default: "TF2G_adj"
-    region_to_gene_key
-        Key under which to find R2G adjacnecies
-        default: "region_to_gene"
-    gsea_n_perm
-        Int specifying number of gsea permutations to run for p value calculation
-        default: 1000
-    quantiles
-        A tuple specifying the quantiles used to binarize region-to-gene links
-        Default: (0.85, 0.90)
-    top_n_regionTogenes_per_gene
-        A tuple specifying the top n region-to-gene links to take PER GENE in order to binarize region-to-gene links.
-        Default: (5, 10, 15)
-    top_n_regionTogenes_per_region
-        A tuple specifying the top n region-to-gene links to take PER REGION in order to binarize region-to-gene links.
-        Default: ()
-    binarize_using_basc:
-        A boolean specifying wether or not to binarize region-to-gene links using BASC. Hopfensitz M, et al.
-    min_regions_per_gene:
-        An integer specifying a lower limit on regions per gene (after binarization) to consider for further analysis.
-        Default: 0
-    rho_dichotomize:
-        A boolean specifying wether or not to split region-to-gene links based on postive/negative correlation coefficients.
-        default: True
-    keep_only_activating:
-        A boolean specifying wether or not to only retain region-to-gene links with a positive correlation coefficient.
-        default: False
-    rho_threshold:
-        A floating point number specifying from which absolute value to consider a correlation coefficient positive or negative.
-        default: 0.03
-    NES_thr
-        Float specifying threshold on gsea NES value
-        defaut: 0
-    adj_pval_thr
-        Float specifying threshold on gsea adjusted p value
-        default: 1
-    min_target_genes
-        Int specifying minumum number of target genes in leading edge
-        default: 5
-    inplace
-        Boolean specifying wether to store results in `SCENICPLUS_obj`
-        default: True
-    key_added
-        Key specifying in under which key to store result in `SCENICPLUS_obj`.uns
-        default: "eRegulons"
-    ray_n_cpu
-        Int specifying number of cores to use
-        default: None
-    merge_eRegulons
-        Boolean specifying wether to merge eRegulons form the same TF but different thresholding approaches
-        default: True
-    keep_extended_motif_annot
-        A boolean specifying wether or not keep extended motif annotations for further analysis.
-        default: False
-    **kwargs
-        Additional keyword arguments passed to `ray.init`
-
-    References
-    ----------
-    Hopfensitz M, et al. Multiscale binarization of gene expression data for reconstructing Boolean networks. IEEE/ACM Trans Comput Biol Bioinform. 2012;9(2):487-98.
-
-    """
-
-    if not adj_key in SCENICPLUS_obj.uns.keys():
-        raise ValueError(
-            f'key {adj_key} not found in uns slot. Please first load TF2G adjacencies!')
-
+def build_grn(
+        tf_to_gene: pd.DataFrame,
+        region_to_gene: pd.DataFrame,
+        cistromes: anndata.AnnData,
+        is_extended: bool,
+        order_regions_to_genes_by='importance',
+        order_TFs_to_genes_by='importance',
+        gsea_n_perm=1000,
+        quantiles=(0.85, 0.90),
+        top_n_regionTogenes_per_gene=(5, 10, 15),
+        top_n_regionTogenes_per_region=(),
+        binarize_using_basc=False,
+        min_regions_per_gene=0,
+        rho_dichotomize_tf2g=True,
+        rho_dichotomize_r2g=True,
+        rho_dichotomize_eregulon=True,
+        keep_only_activating=False,
+        rho_threshold=RHO_THRESHOLD,
+        NES_thr=0,
+        adj_pval_thr=1,
+        min_target_genes=5,
+        ray_n_cpu=None,
+        merge_eRegulons=True,
+        disable_tqdm=False,
+        **kwargs) -> List[eRegulon]:
     log.info('Thresholding region to gene relationships')
     relevant_tfs, e_modules = create_emodules(
-        SCENICPLUS_obj=SCENICPLUS_obj,
-        region_to_gene_key=region_to_gene_key,
+        region_to_gene=region_to_gene,
+        cistromes=cistromes,
+        is_extended=is_extended,
+        order_regions_to_genes_by=order_regions_to_genes_by,
         quantiles=quantiles,
         top_n_regionTogenes_per_gene=top_n_regionTogenes_per_gene,
         top_n_regionTogenes_per_region=top_n_regionTogenes_per_region,
@@ -222,25 +149,20 @@ def build_grn(SCENICPLUS_obj: SCENICPLUS,
         rho_dichotomize=rho_dichotomize_r2g,
         keep_only_activating=keep_only_activating,
         rho_threshold=rho_threshold,
-        keep_extended_motif_annot=keep_extended_motif_annot,
-        order_regions_to_genes_by=order_regions_to_genes_by,
         disable_tqdm=disable_tqdm,
         ray_n_cpu=ray_n_cpu,
-        cistromes_key=cistromes_key,
         **kwargs)
-
     log.info('Subsetting TF2G adjacencies for TF with motif.')
-    TF2G_adj_relevant = SCENICPLUS_obj.uns[adj_key].loc[[
-        tf in relevant_tfs for tf in SCENICPLUS_obj.uns[adj_key]['TF']]]
+    TF2G_adj_relevant = tf_to_gene.loc[tf_to_gene['TF'].isin(relevant_tfs)]
 
     if ray_n_cpu is not None:
         ray.init(num_cpus=ray_n_cpu, **kwargs)
         jobs = []
 
-    log.info(f'Running GSEA...')
-    new_e_modules = []
+    log.info('Running GSEA...')
+    new_e_modules: List[eRegulon] = []
     # dict so adjacencies matrix is only subsetted once per TF (improves performance)
-    TF_to_TF_adj_d = {}
+    TF_to_TF_adj_d:Dict[str, pd.DataFrame] = {}
     tqdm_desc = 'initializing' if ray_n_cpu is not None else 'Running using single core'
     for e_module in tqdm(e_modules, total=len(e_modules), desc=tqdm_desc, disable=disable_tqdm):
         TF = e_module.transcription_factor
@@ -341,7 +263,7 @@ def build_grn(SCENICPLUS_obj: SCENICPLUS,
     for module, adj_pval in zip(new_e_modules, adj_pval):
         module.gsea_adj_pval = adj_pval
 
-    e_modules_to_return = []
+    e_modules_to_return: List[eRegulon] = []
     for module in new_e_modules:
         if module.gsea_adj_pval < adj_pval_thr and module.gsea_enrichment_score > NES_thr:
             module_in_LE = module.subset_leading_edge(inplace=False)
@@ -353,8 +275,4 @@ def build_grn(SCENICPLUS_obj: SCENICPLUS,
             e_modules=e_modules_to_return, inplace=False, rho_dichotomize=rho_dichotomize_eregulon)
     e_modules_to_return = [
         x for x in e_modules_to_return if not isinstance(x, list)]
-    if inplace:
-        log.info(f'Storing eRegulons in .uns[{key_added}].')
-        SCENICPLUS_obj.uns[key_added] = e_modules_to_return
-    else:
-        return e_modules_to_return
+    return e_modules_to_return
