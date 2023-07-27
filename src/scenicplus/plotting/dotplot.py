4d0adb01b79e1a41b51ccafe5e3db4f0aa5e1317
SeppeDeWinter <seppe.dewinter@hotmail.com>
2023-07-27 19:31:26 +0000

    refactor generate dotplot

--- src/scenicplus/plotting/dotplot.py
+++ src/scenicplus/plotting/dotplot.py
@@ -9,126 +9,59 @@ from plotnine import (
     geom_tile, theme, element_text, element_blank)
 from plotnine.facets import facet_grid
 import plotnine
+from typing import List
 
-
-from ..scenicplus_class import SCENICPLUS
-from ..utils import flatten_list
+def _scale(X: pd.DataFrame) -> pd.DataFrame:
+    return (X - X.min()) / (X.max() - X.min())
 
 def generate_dotplot_df(
-    scplus_obj: SCENICPLUS,
     size_matrix: pd.DataFrame,
     color_matrix: pd.DataFrame,
+    group_by: List[str],
+    size_features: List[str], # size_features, color_features and feature_names the order should correspond, e.g. color_features, size_features, feature_names = scplus_mudata.uns["direct_e_regulon_metadata"][["Region_signature_name", "Gene_signature_name", "eRegulon_name"]].drop_duplicates().values.T
+    color_features: List[str],
+    feature_names: List[str],
     scale_size_matrix: bool = True,
     scale_color_matrix: bool = True,
-    group_variable: str = None,
-    subset_eRegulons: list = None) -> pd.DataFrame:
-    """
-    Function to generate dotplot dataframe from cistrome AUC enrichment
-
-    Parameters
-    ----------
-    scplus_obj: `class::SCENICPLUS`
-        A :class:`SCENICPLUS` object.
-    size_matrix: pd.DataFrame
-        A pd.DataFrame containing values to plot using size scale.
-    color_matrix
-        A pd.DataFrame containing values to plot using color scale.
-    scale_size_matrix: bool
-        Scale size matrix between 0 and 1 along index.
-    scale_color_matrix: bool
-        Scale color matrix between 0 and 1 along index.
-    group_variable: str:
-        Variable by which to group cell barcodes by (needed if the index of size or color matrix are cells.)
-    subset_eRegulons: List
-        List of eRegulons to plot.
-    Returns
-    -------
-    pd.DataFrame
-    """
-    # Handle matrices which have cell barcodes on one axis
-    if all(np.isin(size_matrix.columns, scplus_obj.cell_names)):
-        size_matrix = size_matrix.T
-    if all(np.isin(size_matrix.index, scplus_obj.cell_names)):
-        #calculate mean values
-        if group_variable is None:
-            raise ValueError('group_variable can not be None when size_matrix is a matrix with cell barcodes on one axis.')
-        if group_variable not in scplus_obj.metadata_cell.columns:
-            raise ValueError('group variable must be a column in scplus_obj.metadata_cell')
-        size_matrix = size_matrix.groupby(scplus_obj.metadata_cell[group_variable]).mean()
-    if all(np.isin(color_matrix.columns, scplus_obj.cell_names)):
-        color_matrix = color_matrix.T
-    if all(np.isin(color_matrix.index, scplus_obj.cell_names)):
-        #calculate mean values
-        if group_variable is None:
-            raise ValueError('group_variable can not be None when color_matrix is a matrix with cell barcodes on one axis.')
-        if group_variable not in scplus_obj.metadata_cell.columns:
-            raise ValueError('group variable must be a column in scplus_obj.metadata_cell')
-        color_matrix = color_matrix.groupby(scplus_obj.metadata_cell[group_variable]).mean()
-    if not all(np.isin(size_matrix.index, color_matrix.index)):
-        raise ValueError('size_matrix and color_matrix should have the same values as index.')
-    size_matrix_features = (
-        size_matrix.columns.to_list(),                      #full eRegulon name
-        [f.split('_(')[0] for f in size_matrix.columns],    #eRegulon name without number of targets
-        [f.split('_')[0] for f in size_matrix.columns])     #tf name
-    color_matrix_features = (
-        color_matrix.columns.to_list(),                      #full eRegulon name
-        [f.split('_(')[0] for f in color_matrix.columns],    #eRegulon name without number of targets
-        [f.split('_')[0] for f in color_matrix.columns])     #tf name
-
-    #put features in same order
-    #both matrices have eRegulons as features
-    def _find_idx(l, e):
-        return [i for i, v in enumerate(l) if v == e]
-    if all(['_(' in x for x in size_matrix_features[0]]) and all(['_(' in x for x in color_matrix_features[0]]):
-        if not all(np.isin(size_matrix_features[1], color_matrix_features[1])):
-            raise ValueError('When two matrices are given with eRegulons as features then the names of these features (without number of targets) should match!')
-        color_matrix = color_matrix.iloc[
-            :, flatten_list([_find_idx(color_matrix_features[1], e) for e in size_matrix_features[1]])]
-    #size matrix has eRegulons as features but color matrix not
-    elif all(['_(' in x for x in size_matrix_features[0]]):
-        color_matrix = color_matrix[size_matrix_features[2]]
-    #color matrix has eRegulons as features but size matrix not
-    elif all(['_(' in x for x in color_matrix_features[0]]):
-        size_matrix = size_matrix[color_matrix_features[2]]
-    #none of the matrices have eRegulons as features
-    else:
-        size_matrix = size_matrix[color_matrix_features[2]]
-    
-    if subset_eRegulons is not None:
-        #change to TF names
-        subset_eRegulons = [x.split('_')[0] for x in subset_eRegulons]
-        size_matrix = size_matrix[[x for x in size_matrix if x.split('_')[0] in subset_eRegulons]]
-        color_matrix = color_matrix[[x for x in color_matrix if x.split('_')[0] in subset_eRegulons]]
-    
-    if scale_size_matrix:
-        size_matrix = (size_matrix - size_matrix.min()) / (size_matrix.max() - size_matrix.min())
-    if scale_color_matrix:
-        color_matrix = (color_matrix - color_matrix.min()) / (color_matrix.max() - color_matrix.min())
-    
-    size_matrix_df = size_matrix.stack().reset_index()
-    color_matrix_df = color_matrix.stack().reset_index()
-    size_matrix_df.columns = ['index', 'size_name', 'size_val']
-    color_matrix_df.columns = ['index', 'color_name', 'color_val']
-    if all(['_(' in x for x in size_matrix_features[0]]) and all(['_(' in x for x in color_matrix_features[0]]):
-        size_matrix_df['eRegulon_name'] = [x.split('_(')[0] for x in size_matrix_df['size_name']]
-        color_matrix_df['eRegulon_name'] = [x.split('_(')[0] for x in color_matrix_df['color_name']]
-        merged_df = size_matrix_df.merge(color_matrix_df, on = ['index', 'eRegulon_name'])
-        merged_df['TF'] = [x.split('_')[0] for x in merged_df['eRegulon_name']]
-    else:
-        size_matrix_df['TF'] = [x.split('_')[0] for x in size_matrix_df['size_name']]
-        color_matrix_df['TF'] = [x.split('_')[0] for x in color_matrix_df['color_name']]
-        merged_df = size_matrix_df.merge(color_matrix_df, on = ['index', 'TF'])
-        if all(['_(' in x for x in size_matrix_features[0]]):
-            merged_df['eRegulon_name'] = merged_df['size_name']
-        elif all(['_(' in x for x in color_matrix_features[0]]):
-            merged_df['eRegulon_name'] = merged_df['color_name']
-        else:
-            merged_df['eRegulon_name'] = merged_df['TF']
-
-    #for esthetics
-    merged_df = merged_df[['index', 'TF', 'eRegulon_name', 'size_name', 'color_name', 'size_val', 'color_val']]
-    return merged_df
-
+    group_name: str = "group",
+    size_name: str = "size_variable",
+    color_name: str = "color_variable",
+    feature_name: str = "eRegulon") -> pd.DataFrame:
+    # Validate input
+    if not all(size_matrix.index == color_matrix.index):
+        raise ValueError("Both size_matrix and color_matrix should have the same index")
+    if len(group_by) != len(size_matrix.index):
+        raise ValueError("Length of group_by does not match with the index")
+    if len(size_features) != len(color_features) != len(feature_names):
+        raise ValueError("The length of 'size_features', 'color_features' and 'feature_names' should be equal!")
+    # Subset and order for features
+    size_matrix = size_matrix[size_features]
+    color_matrix = color_matrix[color_features]
+    # Calculate mean by group_by variable
+    color_matrix_avg = color_matrix.groupby(group_by).mean()
+    size_matrix_avg = size_matrix.groupby(group_by).mean()
+    # Scale matrices
+    color_matrix_avg = _scale(color_matrix_avg) if scale_color_matrix else color_matrix_avg
+    size_matrix_avg = _scale(size_matrix_avg) if scale_size_matrix else size_matrix_avg
+    # Transform dataframe into long format
+    color_matrix_avg = color_matrix_avg.stack().reset_index()
+    size_matrix_avg = size_matrix_avg.stack().reset_index()
+    color_matrix_avg.columns = [group_name, "color_features", color_name]
+    size_matrix_avg.columns = [group_name, "size_features", size_name]
+    # map between color and size feature names to feature names
+    color_features_to_name = dict(zip(color_features, feature_names))
+    size_features_to_name = dict(zip(size_features, feature_names))
+    # Add feature names to dataframe
+    color_matrix_avg[feature_name] = [
+        color_features_to_name[f] for f in color_matrix_avg["color_features"]]
+    size_matrix_avg[feature_name] = [
+        size_features_to_name[f] for f in size_matrix_avg["size_features"]]
+    color_matrix_avg = color_matrix_avg.drop("color_features", axis = 1)
+    size_matrix_avg = size_matrix_avg.drop("size_features", axis = 1)
+    dotplot_df = color_matrix_avg.merge(
+        size_matrix_avg,
+        on = [group_name, feature_name])
+    return dotplot_df
 
 def heatmap_dotplot(
     scplus_obj: SCENICPLUS,
@@ -144,36 +77,7 @@ def heatmap_dotplot(
     figsize: tuple = (5, 8),
     split_repressor_activator: bool = True,
     orientation: str = 'vertical'):
-    """
-    Function to generate dotplot dataframe from cistrome AUC enrichment
-
-    Parameters
-    ----------
-    scplus_obj: `class::SCENICPLUS`
-        A :class:`SCENICPLUS` object.
-    size_matrix: pd.DataFrame
-        A pd.DataFrame containing values to plot using size scale.
-    color_matrix
-        A pd.DataFrame containing values to plot using color scale.
-    scale_size_matrix: bool
-        Scale size matrix between 0 and 1 along index.
-    scale_color_matrix: bool
-        Scale color matrix between 0 and 1 along index.
-    group_variable: str:
-        Variable by which to group cell barcodes by (needed if the index of size or color matrix are cells.)
-    subset_eRegulons: List
-        List of eRegulons to plot.
-    sort_by: str
-        Sort by color_val or size_val.
-    index_order: list
-        Order of index to plot.
-    figsize: tuple
-        size of the figure (x, y).
-    split_repressor_activator: bool
-        Wether to split the plot on repressors/activators.
-    orientation: str
-        Plot in horizontal or vertical orientation
-    """
+   
     plotting_df = generate_dotplot_df(
         scplus_obj = scplus_obj,
         size_matrix = size_matrix,
