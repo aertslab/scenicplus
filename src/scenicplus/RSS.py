fe3586c1d69e2f1d24bbc75960517fa37caae9b6
eceksi <erencan.eksi@kuleuven.be>
2024-02-15 14:01:36 +0100

    Update RSS.py

--- src/scenicplus/RSS.py
+++ src/scenicplus/RSS.py
@@ -1,6 +1,8 @@
 """Calculate the specificty of eRegulons in clusters of cells.
 
-Calculates the distance between the real distribution of eRegulon AUC values and a fictional distribution where the eRegulon is only expressed/accessible in cells of a certain cluster.
+Calculates the distance between the real distribution of eRegulon AUC values and
+a fictional distribution where the eRegulon is only expressed/accessible in cells
+of a certain cluster.
 
 """
 
@@ -10,94 +12,103 @@ from scipy.spatial.distance import jensenshannon
 from math import ceil, floor
 import matplotlib.pyplot as plt
 import matplotlib
-from typing import List, Tuple
-from typing import Optional
+from typing import List, Tuple, Union
 from adjustText import adjust_text
-import sklearn
+from mudata import MuData
+from scenicplus.scenicplus_mudata import ScenicPlusMuData
 
-from .scenicplus_class import SCENICPLUS
+def rss(aucs, labels):
+        # jensenshannon function provides distance 
+        # which is the sqrt of the JS divergence.
+        return 1.0 - jensenshannon(aucs / aucs.sum(), labels / labels.sum())
 
+def regulon_specificity_scores_df(
+                               data_matrix: pd.DataFrame,
+                               variable_matrix: pd.DataFrame):
 
-def regulon_specificity_scores(scplus_obj: SCENICPLUS,
-                               variable: str,
-                               auc_key: Optional[str] = 'eRegulon_AUC',
-                               signature_keys: Optional[List[str]] = ['Gene_based', 'Region_based'],
-                               selected_regulons: Optional[List[int]] = None,
-                               scale: Optional[bool] = False,
-                               out_key_suffix: Optional[str] = ''):
     """
     Calculate the Regulon Specificty Scores (RSS). [doi: 10.1016/j.celrep.2018.10.045]
 
     Parameters
     ---------
-    scplus_obj: `class::SCENICPLUS`
-        A SCENICPLUS object with eRegulons AUC computed.
-    variable: str
-        Variable to calculate the RSS values for.
-    auc_key: str, optional
-        Key to extract AUC values from. Default: 'eRegulon_AUC'
-    signature_keys: List, optional
-        Keys to extract AUC values from. Default: ['Gene_based', 'Region_based']
-    scale: bool, optional
-        Whether to scale the enrichment prior to the clustering. Default: False
-    out_key_suffix: str, optional
-        Suffix to add to the variable name to store the values (at scplus_obj.uns['RSS'])
+    data_matrix: 'class::pd.DataFrame`
+        A pandas dataframe containing regulon scores per cell.
+    variable_matrix: 'class::pd.DataFrame'
+        A pandas dataframe with an annotation per cell.
     """
-    if scale:
-        data_mat = pd.concat([pd.DataFrame(sklearn.preprocessing.StandardScaler().fit_transform(
-            scplus_obj.uns[auc_key][x].T), index=scplus_obj.uns[auc_key][x].T.index.to_list(), columns=scplus_obj.uns[auc_key][x].T.columns) for x in signature_keys]).T
-    else:
-        data_mat = pd.concat([scplus_obj.uns[auc_key][x]
-                             for x in signature_keys], axis=1)
 
-    if selected_regulons is not None:
-        subset = [x for x in selected_regulons if x in data_mat.columns]
-        data_mat = data_mat[subset]
-
-    cell_data_series = scplus_obj.metadata_cell.loc[data_mat.index, variable]
-    cell_data = list(cell_data_series.unique())
-    n_types = len(cell_data)
-    regulons = list(data_mat.columns)
+    cell_types = list(variable_matrix.unique())
+    n_types = len(cell_types)
+    regulons = list(data_matrix.columns)
     n_regulons = len(regulons)
-    rss_values = np.empty(shape=(n_types, n_regulons), dtype=np.float)
-
-    def rss(aucs, labels):
-        # jensenshannon function provides distance which is the sqrt of the JS divergence.
-        return 1.0 - jensenshannon(aucs / aucs.sum(), labels / labels.sum())
+    rss_values = np.empty(shape=(n_types, n_regulons), dtype=float)
 
     for cidx, regulon_name in enumerate(regulons):
-        for ridx, type in enumerate(cell_data):
+        for ridx, type in enumerate(cell_types):
             rss_values[ridx, cidx] = rss(
-                data_mat[regulon_name], (cell_data_series == type).astype(int))
+                data_matrix[regulon_name], (variable_matrix == type).astype(int))
 
     rss_values = pd.DataFrame(
-        data=rss_values, index=cell_data, columns=regulons)
+        data=rss_values, index=cell_types, columns=regulons)
 
-    if not 'RSS' in scplus_obj.uns.keys():
-        scplus_obj.uns['RSS'] = {}
-    out_key = variable + out_key_suffix
-    if not out_key in scplus_obj.uns['RSS'].keys():
-        scplus_obj.uns['RSS'][out_key] = {}
-    scplus_obj.uns['RSS'][out_key] = rss_values
+    return rss_values
+
+def regulon_specificity_scores(scplus_mudata: Union[MuData, ScenicPlusMuData],
+                               variable: str,
+                               modalities: list,
+                               selected_regulons: List[int] = None):
+    """
+    Calculate the Regulon Specificty Scores (RSS). [doi: 10.1016/j.celrep.2018.10.045]
+
+    Parameters
+    ---------
+    scplus_mudata: `class::MuData` or 'class::ScenicPlusMuData'
+        A MuData object with eRegulons AUC computed.
+    variable: str
+        Variable to calculate the RSS values for.
+    modalities: List,
+        A list of modalities to calculate RSS values for.
+    selected_regulons: List, optional
+        Regulons to calculate RSS values for.
+    """
+
+    #TODO: add checks
+
+    rss_values_per_modality = []
+
+    for modality in modalities:
+        if selected_regulons is not None:
+            modality_regulons = [regulon for regulon in selected_regulons 
+            if regulon in scplus_mudata.mod[modality].var_names]
+            
+        else:
+            modality_regulons = list(scplus_mudata.mod[modality].var_names)
+        data_matrix = scplus_mudata.mod[modality][:, modality_regulons].to_df()
+
+        variable_matrix = scplus_mudata.obs.loc[data_matrix.index, variable]
+
+        rss_values_per_modality.append(regulon_specificity_scores_df(data_matrix=data_matrix,
+                                         variable_matrix=variable_matrix))
+    
 
 
-def plot_rss(scplus_obj: SCENICPLUS,
-             rss_key: str,
-             top_n: Optional[int] = 5,
-             selected_groups: Optional[List[str]] = None,
-             num_columns: Optional[int] = 1,
-             figsize: Optional[Tuple[float, float]] = (6.4, 4.8),
-             fontsize: Optional[int] = 12,
+    return pd.concat(rss_values_per_modality, axis=1)
+
+
+def plot_rss(data_matrix: pd.DataFrame,
+             top_n: int = 5,
+             selected_groups: List[str] = None,
+             num_columns: int = 1,
+             figsize: Tuple[float, float] = (6.4, 4.8),
+             fontsize: int = 12,
              save: str = None):
     """
     Plot RSS values per group
 
     Parameters
     ---------
-    scplus_obj: `class::SCENICPLUS`
-        A SCENICPLUS object with eRegulons AUC computed.
-    rss_key: str, optional
-        Key to extract RSS values from.
+    data_matrix: `class::pd.DataFrame`
+        A pandas dataframe with RSS scores per variable.
     top_n: int, optional
         Number of top eRegulons to highlight.
     selected_groups: List, optional
@@ -105,16 +116,17 @@ def plot_rss(scplus_obj: SCENICPLUS,
     num_columns: int, optional
         Number of columns for multiplotting
     figsize: tuple, optional
-        Size of the figure. If num_columns is 1, this is the size for each figure; if num_columns is above 1, this is the overall size of the figure (if keeping
+        Size of the figure. If num_columns is 1, this is the size for each figure;
+        if num_columns is above 1, this is the overall size of the figure (if keeping
         default, it will be the size of each subplot in the figure). Default: (6.4, 4.8)
     fontsize: int, optional
         Size of the eRegulons names in plot.
     save: str, optional
         Path to save plot. Default: None.
     """
-    data_mat = scplus_obj.uns['RSS'][rss_key]
+
     if selected_groups is None:
-        cats = sorted(data_mat.index.tolist())
+        cats = sorted(data_matrix.index.tolist())
     else:
         cats = selected_groups
 
@@ -130,14 +142,14 @@ def plot_rss(scplus_obj: SCENICPLUS,
         pdf = matplotlib.backends.backend_pdf.PdfPages(save)
 
     for c in cats:
-        x = data_mat.T[c]
+        x = data_matrix.T[c]
         if num_columns > 1:
             ax = fig.add_subplot(num_rows, num_columns, i)
             i = i + 1
         else:
             fig = plt.figure(figsize=figsize)
             ax = plt.axes()
-        _plot_rss_internal(data_mat, c, top_n=top_n, max_n=None, ax=ax)
+        _plot_rss_internal(data_matrix, c, top_n=top_n, max_n=None, ax=ax)
         ax.set_ylim(x.min()-(x.max()-x.min())*0.05,
                     x.max()+(x.max()-x.min())*0.05)
         for t in ax.texts:
@@ -184,7 +196,6 @@ def plot_rss(scplus_obj: SCENICPLUS,
     if (save is not None) & (num_columns == 1):
         pdf = pdf.close()
 
-
 def _plot_rss_internal(rss, cell_type, top_n=5, max_n=None, ax=None):
     """
     Helper function to plot RSS
@@ -205,7 +216,9 @@ def _plot_rss_internal(rss, cell_type, top_n=5, max_n=None, ax=None):
         'weight': 'normal'
     }
 
-    for idx, (regulon_name, rss_val) in enumerate(zip(data[0:top_n].index, data[0:top_n].values)):
+    for idx, (regulon_name, rss_val) in enumerate(
+        zip(data[0:top_n].index, data[0:top_n].values)):
+
         ax.plot([idx, idx], [rss_val, rss_val], 'r.')
         ax.text(
             idx + (max_n / 25),
