c1924d7b03ca80b7bea3215e204e12bc1dca3124
sdewin <seppe.dewinter@hotmail.com>
2024-05-07 13:01:18 +0200

    Fix versions.

--- setup.py
+++ setup.py
@@ -13,20 +13,19 @@ dependency_links = []
 # Do not add to required lines pointing to Git repositories
 EGG_MARK = '#egg='
 for line in requirements:
-	if line.startswith('-e git:') or line.startswith('-e git+') or \
-		line.startswith('git:') or line.startswith('git+'):
-		line = line.lstrip('-e ')  # in case that is using "-e"
-		if EGG_MARK in line:
-			package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
-			repository = line[:line.find(EGG_MARK)]
-			required.append('%s @ %s' % (package_name, repository))
-			dependency_links.append(line)
-		else:
-			print('Dependency to a git repository should have the format:')
-			print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
-	else:
-		required.append(line)
-
+    if line.startswith('-e git:') or line.startswith('-e git+') or \
+        line.startswith('git:') or line.startswith('git+'):
+        line = line.lstrip('-e ')  # in case that is using "-e"
+        if EGG_MARK in line:
+            package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
+            repository = line[:line.find(EGG_MARK)]
+            required.append('%s @ %s' % (package_name, repository))
+            dependency_links.append(line)
+        else:
+            print('Dependency to a git repository should have the format:')
+            print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
+    else:
+        required.append(line)
 
 setuptools.setup(
      name='scenicplus',
